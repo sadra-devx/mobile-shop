df2d76ec1104b790876ee964b2714e993b04b0db
sadra-devx <sadra.apex@gmail.com>
2026-07-24 12:13:12 +0330

    fix: resolve merge conflict in Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,36 +1,18 @@
-<<<<<<< Updated upstream
-import Hero from "../../components/Hero/Hero"
-=======
-import { CategorySection } from "../../components/Home/CategorySection"
-import Hero from "../../components/Home/Hero/Hero"
-import { WhyUs } from "../../components/Home/WhyUs/WhyUs"
-import { FeaturedProducts } from "../../components/Product/FeaturedProducts"
->>>>>>> Stashed changes
+// src/pages/Home/Home.jsx
+import { CategorySection } from "../../components/Home/CategorySection";
+import Hero from "../../components/Home/Hero/Hero";
+import { WhyUs } from "../../components/Home/WhyUs/WhyUs";
+import { FeaturedProducts } from "../../components/Product/FeaturedProducts";
 
 function Home() {
   return (
-    <div className="w-full">
+    <div className="w-full bg-zinc-50 dark:bg-zinc-900">
       <Hero />
-<<<<<<< Updated upstream
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-=======
       <CategorySection />
       <FeaturedProducts />
       <WhyUs />
->>>>>>> Stashed changes
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
