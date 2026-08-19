78c1424def17b7618134a6b44351f0a420cc096b
sadra-devx <sadra.apex@gmail.com>
2026-08-19 20:16:28 +0330

    feat: wrap app with BasketProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
-import { router } from "./routes/routes"; 
+import { router } from "./routes/routes";
 import "./index.css";
 import "@fontsource/vazirmatn/400.css";
 import "@fontsource/vazirmatn/500.css";
 import "@fontsource/vazirmatn/700.css";
+import BasketProvider from "./context/BasketContext";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <BasketProvider>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </BasketProvider>,
 );
